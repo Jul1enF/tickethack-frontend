b4cb1e378a1722ac34219a40e4f1fa37be64b295
Jul1enF <j.furic@gmail.com>
2024-07-03 14:00:53 +0200

    rajout de l'affichage des achats dans bookings.js

---
+++ bookings.js
@@ -0,0 +1,30 @@
+function displayAllTrip(data){
+    if (data.trips[0].name){
+        document.querySelector('#bookingContainer').innerHTML=''
+        document.querySelector('#enjoyContainer').style.display="flex"
+    
+    
+        for (let i=0; i<data.trips.length ; i++){
+            let currentTime = new Date()
+            let departureTime = data.trips[i].trip.date
+            let hours = (Date.parse(departureTime)-Date.parse(currentTime))/1000/60/60
+            hours = Math.floor(hours)
+            
+            document.querySelector('#bookingContainer').innerHTML+=`<div           class="tripLine"> 
+                    <span> ${data.trips[i].name}</span>
+                    <span> ${data.trips[i].time}</span>
+                    <span> ${data.trips[i].trip.price}€</span>
+                    <span>Departure in ${hours} hours</span> 
+                    `
+        }
+        
+    }
+        
+}
+
+
+fetch('http://localhost:3000/cart')
+.then(response=>response.json())
+.then(data => {
+    displayAllTrip(data)
+})
